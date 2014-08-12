07f97b930fab2989b0e2a1c3a8c25ce709987b3c
Pieter Soudan <pieter.soudan@redandivory.be>
2014-08-12 15:45:41 +0200

    fix reference to raphaeljs

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['jquery', 'raphael'], 'client');
+  api.use(['jquery', 'raphaeljs'], 'client');
   api.add_files('lib/build/sequence-diagram-min.js', 'client');
   api.add_files('export.js', 'client');
   api.export('Diagram');
